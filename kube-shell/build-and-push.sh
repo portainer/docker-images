46835e54fe5807b332b4e6177e8179a75a4aa59b
waysonwei <degui.wei@gmail.com>
2021-09-15 12:04:03 +1200

    changes required from tech review for tag

--- kube-shell/build-and-push.sh
+++ kube-shell/build-and-push.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 set -e
 
-: ${REPO:=portainer/kubectl-shell}
+: ${REPO:=$1/kubectl-shell}
 : ${KUBERNETES_RELEASE:=v1.21.1}
-: ${TAG:=latest}
+: ${TAG:=$2}
 
 docker_image_build_and_push()
 {
   docker buildx build -o type=docker \
     --build-arg ARCH=${1?required} \
     --build-arg ALPINE=${2?required} \
-    --build-arg KUBERNETES_RELEASE=${3?required} \
+    --build-arg KUBERNETES_RELEASE=${4?required} \
     --platform linux/${1} \
-    --tag ${4?required}:${3}-${1} \
-  ${5?required}
-  docker image push ${4?required}:${3}-${1}
+    --tag ${5?required}:${3}-${4}-${1} \
+  ${6?required}
+  docker image push ${5?required}:${3}-${4}-${1}
 }
 
 docker_manifest_create_and_push()
@@ -27,8 +27,8 @@ docker_manifest_create_and_push()
   docker manifest push $2
 }
 
+docker_image_build_and_push amd64  amd64/alpine:latest   ${TAG} ${KUBERNETES_RELEASE} ${REPO} $(dirname $0)/.
+docker_image_build_and_push arm64  arm64v8/alpine:latest ${TAG} ${KUBERNETES_RELEASE} ${REPO} $(dirname $0)/.
+docker_image_build_and_push arm    arm32v7/alpine:latest ${TAG} ${KUBERNETES_RELEASE} ${REPO} $(dirname $0)/.
 
-docker_image_build_and_push amd64  amd64/alpine:latest   ${KUBERNETES_RELEASE} ${REPO} $(dirname $0)/.
-docker_image_build_and_push arm64  arm64v8/alpine:latest ${KUBERNETES_RELEASE} ${REPO} $(dirname $0)/.
-
-docker_manifest_create_and_push ${REPO}:${KUBERNETES_RELEASE} ${REPO}:${TAG} 
+docker_manifest_create_and_push ${REPO}:${TAG}-${KUBERNETES_RELEASE} ${REPO}:${TAG} 
